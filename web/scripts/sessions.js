9eba4b217bc8978967031f280d7c48ebc191209b
TychoQS <tycho.quintana@gmail.com>
2025-03-11 18:32:59 +0000

    Added pseudo login validation

--- web/scripts/sessions.js
+++ web/scripts/sessions.js
@@ -1,17 +1,22 @@
-document.getElementById("login-submit").addEventListener("onclick", function(e) {
-    e.preventDefault();
-    console.log("login submit");
-})
+function addLoginSubmitEventListener() {
+    setTimeout(function() {
+        const loginSubmit = document.getElementById("login-submit");
+        loginSubmit.addEventListener("click", ev => {
+            ev.preventDefault();
+            Login();
+
+        })
+    }, 1000);
+}
 
 
-function checkLogin() {
+function Login() {
     const mail = document.getElementById("login_email").value;
     const password = document.getElementById("login_password").value;
     console.log(mail, password);
     fetch("../json/data/user.json")
         .then(response => response.json())
         .then(userData => {
-            console.log(userData["email"] === mail && userData["password"] === password);
             return userData["email"] === mail && userData["password"] === password;
         })
         .catch(error => console.log(error));
