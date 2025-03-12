4df933370e00625022348530b82df37f9abd26e6
TychoQS <tycho.quintana@gmail.com>
2025-03-12 00:43:27 +0000

    Added some validation to log in

--- web/scripts/sessions.js
+++ web/scripts/sessions.js
@@ -1,25 +1,46 @@
 function addLoginSubmitEventListener() {
     setTimeout(function() {
+        addClearValidationErrorsWhenInput() // [TODO REMOVE THIS COMMENT] Just here to use the timeout
         const loginSubmit = document.getElementById("login_form");
         loginSubmit.addEventListener("submit", ev => {
             ev.preventDefault();
-            const mail = document.getElementById("login_email").value;
-            const password = document.getElementById("login_password").value;
+            const mail = document.getElementById("login_email");
+            const password = document.getElementById("login_password");
             isRegistered(mail, password).then(isLogged => {
                 if (isLogged) {
                     sessionStorage.setItem("login", mail);
+                    location.href = "../pages/index.html";
                 }
             });
         })
     }, 1000);
 }
 
-
 function isRegistered(mail, password) {
     return fetch("../json/data/user.json")
         .then(response => response.json())
         .then(userData => {
-            return (userData["email"] === mail && userData["password"] === atob(password));
+            if (userData["email"] !== mail.value) {
+                mail.setCustomValidity("This address is not registered.");
+                mail.reportValidity();
+                return false;
+            }
+            if (userData["password"] !== atob(password.value)) {
+                password.setCustomValidity("This password is not correct.");
+                password.reportValidity();
+                return false;
+            }
+            return true;
         })
         .catch(error => console.log(error));
+}
+
+function addClearValidationErrorsWhenInput() {
+    document.getElementById("login_email").addEventListener("input", () => clearValidationErrors());
+    document.getElementById("login_password").addEventListener("input", () => clearValidationErrors());
+}
+
+function clearValidationErrors() {
+    document.getElementById("login_email").setCustomValidity("");
+    document.getElementById("login_password").setCustomValidity("");
 }
