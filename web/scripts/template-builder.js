5897b622c51d25a80fe9cb4c9bfa65444a591bc5
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-03-12 13:15:27 +0000

    First version of template loading with js

--- web/scripts/template-builder.js
+++ web/scripts/template-builder.js
@@ -1,23 +1,44 @@
+// MAIN
+
+buildAllPageTemplates();
 
 // FUNCTIONS
 
-function buildPageFromTemplates(){
+function buildAllPageTemplates(){
     const templateList = document.querySelectorAll("template");
     for (const template of templateList){
-        const templateClone = template.cloneNode(true);
+        const templateDocumentFragment = template.content.cloneNode(true);
         const templateTargetID = template.getAttribute("target-id");
-        const contentDataFileName = templateClone.getAttribute("content-data-file");
+        const templateTargetElement = document.querySelector(`#${templateTargetID}`);
+        const contentDataFileName = template.getAttribute("data-content-file");
 
-        (!contentDataFileName) ? document.querySelector(templateTargetID).appendChild(templateClone) :
+        (!contentDataFileName) ? templateTargetElement.appendChild(templateDocumentFragment) :
         fetchContentDataFromFile(contentDataFileName)
-            .then(contentData => buildNodeContent(templateClone, contentData))
-            .then(templateCloneWithContentData => document.querySelector(templateTargetID).appendChild(templateCloneWithContentData));
+            .then(contentDataFileJson => buildAllTemplatesFromJson(templateDocumentFragment, contentDataFileJson, contentDataFileName))
+            .then(templateWithContentDataDocumentFragment =>
+                templateTargetElement.appendChild(templateWithContentDataDocumentFragment)
+            );
     }
+}
+
+// TODO: No añade todos los templates con los datos de json, solo el primero
+function buildAllTemplatesFromJson(templateDocumentFragment, contentDataFileJson, contentDataFileName){
+    const contentDataDocumentFragment = document.createDocumentFragment();
+
+    for (const contentData of contentDataFileJson[contentDataFileName]) {
+        console.log(contentData);
+        for (const child of templateDocumentFragment.children) {
+
+            contentDataDocumentFragment.appendChild(buildNodeContent(child, contentData));
+        }
+    }
+
+    return contentDataDocumentFragment;
 }
 
 
 function fetchContentDataFromFile(contentDataFileName) {
-    return fetch(`../json/${contentDataFileName}.json`)
+    return fetch(`../json/data/${contentDataFileName}.json`)
         .then(response => response.json()).
         catch(error => {
             console.error(`Error fetching data from ${contentDataFileName} file: `, error)
@@ -25,14 +46,16 @@ function fetchContentDataFromFile(contentDataFileName) {
         });
 }
 
-
 function buildNodeContent(node, nodeContentData){
     if (!nodeContentData) return node;
-    /*
 
-        Fill with content data
+    const contentDataKey = node.getAttribute("data-content-key");
+    // TODO: En (../pages/template-loading-test.html) no carga el text de "artist-caption"
+    if (contentDataKey){
+        if (contentDataKey === "image") node.setAttribute("src", nodeContentData[contentDataKey]);
+        else node.innerText = nodeContentData[contentDataKey];
+    }
 
-    */
-    for (const child of node.children) node.append(buildNodeContent(child));
+    for (const child of node.children) node.append(buildNodeContent(child, nodeContentData));
     return node;
 }
