879cfdf66cde93dc0553597ff81e4ee39735a791
TychoQS <tycho.quintana@gmail.com>
2025-03-13 03:59:40 +0000

    Now header dinamically changes when user logs in or logs out

--- web/scripts/global.js
+++ web/scripts/global.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function updateTemplatesIfLogged() {
         if (! isUserLogged()) return;
-        document.body.style.visibility = "hidden"; // Hiding body to don't show previous templates if is unnecesary
+        document.body.style.visibility = "hidden"; // Hiding body while we are dinamically changing templates
         updateHeader()
             .then(() => document.body.style.visibility = "visible");
 }
@@ -13,57 +13,36 @@ function updateTemplatesIfLogged() {
 function isUserLogged() {
     return sessionStorage.getItem("login") != null;
 }
-
 async function updateHeader() {
-    let header = document.querySelector("header");
-    let currentHeaderTemplate = header.getAttribute("xlu-include-file");
     const template = await fetchUserIconTemplate();
-    const newHeaderTemplate = await fetchNewHeader(currentHeaderTemplate);
-    let doc
-    if (currentHeaderTemplate === "../templates/header.html") doc = createDocWithLoggedMainHeader(newHeaderTemplate, template);
-    if (currentHeaderTemplate === "../templates/second-header.html") doc = createDocWithLoggedSecondaryHeader(newHeaderTemplate, template);
-    let newHeader = createNewHeaderIn(doc);
-    header = document.querySelector("header");
-    replaceInBody(header, newHeader);
-
-    function createDocWithLoggedMainHeader(newHeaderTemplate, template) {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(newHeaderTemplate, "text/html");
-        let nav = doc.querySelector("#header_right-nav");
-        nav.innerHTML = template;
-        return doc;
+    let header = document.querySelector("header");
+    let headerType = getHeaderType(header);
+    if (headerType === "second-header") {
+        updateWithSecondHeader(header, template);
     }
+    if (headerType === "header") {
+        updateWithMainHeader(header, template);
+    }
+
 }
 
-function createDocWithLoggedSecondaryHeader(newHeaderTemplate, template) {
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(newHeaderTemplate, "text/html");
-    let nav = doc.querySelector("#header_right-nav2");
-    nav.innerHTML = template;
-    return doc;
+function getHeaderType(header) {
+    let headerType = header.getAttribute("template-id");
+    return headerType;
 }
 
-
-function createNewHeaderIn(doc) {
-    console.log(doc);
-    let newHeader = document.createElement("header");
-    console.log(newHeader);
-    newHeader.innerHTML = doc.body.innerHTML;
-    console.log("Tras inner");
-    console.log(newHeader);
-    return newHeader;
+function updateWithSecondHeader(header, template) {
+    let buttonToRemove = header.querySelector("#account-section");
+    buttonToRemove.remove();
+    header.insertAdjacentHTML("beforeend", template);
 }
 
-function replaceInBody(header, newHeader) {
-    const body = header.parentNode;
-    body.replaceChild(newHeader, header);
+function updateWithMainHeader(header, template) {
+    let right_nav = header.querySelector("#header_right-nav");
+    right_nav.innerHTML = template;
 }
 
 function fetchUserIconTemplate() {
     return fetch("../templates/user-icon.html")
-        .then(res => res.text());
-}
-function fetchNewHeader(newHeader) {
-    return fetch(newHeader)
         .then(res => res.text());
 }
