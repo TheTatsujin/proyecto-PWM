0407edfacca664d6c3d56595d87220595f280e4e
DylanCas16 <dylancastrillon@outlook.com>
2025-03-13 09:50:43 +0000

    Updated register.js.

--- web/scripts/register.js
+++ web/scripts/register.js
@@ -1,20 +1,34 @@
+function clearValidationsListener() {
+    setTimeout(function () {
+        addClearValidationErrorsWhenInput();
+    }, 1000);
+}
+
 function addRegisterListener() {
     setTimeout(function () {
-        const register = document.getElementById("register-submit");
-        register.addEventListener("click", ev => {
+        const register = document.getElementById("register-form");
+        register.addEventListener("submit", ev => {
             ev.preventDefault();
             const email = document.getElementById("register-email");
             const phoneNumber = document.getElementById("phone-number");
             const password = document.getElementById("password");
             const passwordConfirm = document.getElementById("confirm-password");
-            checkRegister(email, phoneNumber, password, passwordConfirm);
+            if (checkRegister(email, phoneNumber, password, passwordConfirm)) {
+                location.href = "../pages/index.html";
+            }
         })
     }, 1000);
 }
 
 function checkRegister(email, phoneNumber, password, passwordConfirm) {
 
-    if (password !== passwordConfirm) {
+    if (!/[0-9]{3}[- ]?[0-9]{3}[- ]?[0-9]{3}/.test(phoneNumber)) {
+        phoneNumber.setCustomValidity("Phone number must be only 9 numeric digits.");
+        phoneNumber.reportValidity();
+        return false;
+    }
+
+    if (password.value !== passwordConfirm.value) {
         passwordConfirm.setCustomValidity("Password and confirmation fields must coincide.");
         passwordConfirm.reportValidity();
         return false;
@@ -25,19 +39,20 @@ function checkRegister(email, phoneNumber, password, passwordConfirm) {
         password.reportValidity();
         return false;
     }
+
     return true;
 
 }
 
 function addClearValidationErrorsWhenInput() {
-    document.getElementById("register_email").addEventListener("input", () => clearValidationErrors());
-    document.getElementById("password").addEventListener("input", () => clearValidationErrors());
-    document.getElementById("confirm-password").addEventListener("input", () => clearValidationErrors());
-    document.getElementById("phone-number").addEventListener("input", () => clearValidationErrors());
+    document.getElementById("register-email").addEventListener("submit", () => clearValidationErrors());
+    document.getElementById("password").addEventListener("submit", () => clearValidationErrors());
+    document.getElementById("confirm-password").addEventListener("submit", () => clearValidationErrors());
+    document.getElementById("phone-number").addEventListener("submit", () => clearValidationErrors());
 }
 
 function clearValidationErrors() {
-    document.getElementById("register_email").setCustomValidity("");
+    document.getElementById("register-email").setCustomValidity("");
     document.getElementById("password").setCustomValidity("");
     document.getElementById("confirm-password").setCustomValidity("");
     document.getElementById("phone-number").setCustomValidity("");
