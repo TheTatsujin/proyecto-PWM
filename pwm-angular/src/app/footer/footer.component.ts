aa1d7272a85c16d91bab737ce15e3bb189611346
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-04-01 16:25:24 +0100

    Footer component Ts update

--- pwm-angular/src/app/footer/footer.component.ts
+++ pwm-angular/src/app/footer/footer.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  // Urls
+  @Input() instagramLink: string = "#";
+  @Input() tiktokLink: string = "#";
+  @Input() youtubeLink: string = "#";
+  @Input() twitterLink: string = "#";
+
+  // Icons
+  @Input() instagramIconLink: string = "#";
+  @Input() tiktokIconLink: string = "#";
+  @Input() youtubeIconLink: string = "#";
+  @Input() twitterIconLink: string = "#";
+
+  @Input() telephoneIconLink: string = "";
+  @Input() messageIconLink: string = "";
+  @Input() emailIconLink: string = "";
+
+
+  // Contact info
+  @Input() telephoneNumber: string = "";
+  @Input() messageNumber: string = "";
+  @Input() email: string = "";
 
 }
