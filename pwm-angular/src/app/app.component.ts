d571b0c2cc31d238421c1572d429c2d981659c9e
TychoQS <tycho.quintana@gmail.com>
2025-04-01 23:42:22 +0100

    Typo

--- pwm-angular/src/app/app.component.ts
+++ pwm-angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import {DropdownComponent} from './dropdown/dropdown.component';
+import {LoginFormComponent} from './login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DropdownComponent],
+  imports: [LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
