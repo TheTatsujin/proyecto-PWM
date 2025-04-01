3712a337bb3cba2545c5529325f61ced68ca8fe3
DylanCas16 <dylancastrillon@outlook.com>
2025-04-01 17:48:41 +0100

    Created accordion and artist banner components

---
+++ pwm-angular/src/app/artist-banner/artist-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-banner',
+  imports: [],
+  templateUrl: './artist-banner.component.html',
+  styleUrl: './artist-banner.component.css'
+})
+export class ArtistBannerComponent {
+
+}
