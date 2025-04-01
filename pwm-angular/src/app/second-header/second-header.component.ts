415ad84940ec3417523d32ca8450936571ba5798
DylanCas16 <dylancastrillon@outlook.com>
2025-04-01 17:31:25 +0100

    Added headers components

---
+++ pwm-angular/src/app/second-header/second-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-header',
+  imports: [],
+  templateUrl: './second-header.component.html',
+  styleUrl: './second-header.component.css'
+})
+export class SecondHeaderComponent {
+
+}
