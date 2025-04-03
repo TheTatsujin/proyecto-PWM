04068f4e4a171e2d1793662a1dc0dd7e3408ebb7
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-04-03 22:41:21 +0100

    Before pull merge

--- pwm-angular/src/app/app.component.ts
+++ pwm-angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
+import {ArtistBannerComponent} from "./artist-banner/artist-banner.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, ArtistBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
