3d72fcb235e460721a11cc20eab19a22ba030de2
DylanCas16 <dylancastrillon@outlook.com>
2025-04-03 19:39:43 +0100

    Created register form page.

---
+++ pwm-angular/src/app/register-form/register-form.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ReturnButtonComponent} from '../return-button/return-button.component';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register-form',
+  imports: [CommonModule, ReturnButtonComponent, MatCheckboxModule, ReactiveFormsModule],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.css'
+})
+export class RegisterFormComponent {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group( {
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required]),
+      birthdate: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirm: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get name() { return this.registerForm.get('name'); }
+  get email() { return this.registerForm.get('email'); }
+  get phone() { return this.registerForm.get('phone'); }
+  get birthdate() { return this.registerForm.get('birthdate'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirm() { return this.registerForm.get('confirm'); }
+
+
+}
