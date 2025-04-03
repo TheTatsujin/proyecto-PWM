a2f6ad897ad40c42527c5dc00ad0cb73a6fba549
TychoQS <tycho.quintana@gmail.com>
2025-04-03 01:14:43 +0100

    Added reactive login form

--- pwm-angular/src/app/dropdown/dropdown.component.ts
+++ pwm-angular/src/app/dropdown/dropdown.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-dropdown',
   imports: [
     ReactiveFormsModule,
-    NgForOf
   ],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.css'
