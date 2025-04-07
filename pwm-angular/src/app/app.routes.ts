dcc5cd24fb3d66e8ca44ba20adcd8ddb9b8ec067
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-04-07 19:52:00 +0100

    Added main page component

--- pwm-angular/src/app/app.routes.ts
+++ pwm-angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-  path: '',
-  pathMatch: 'full',
-  loadComponent: () => {
-    return import("./components/pages/main/main-page.component").then((m) => m.MainPageComponent);
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import("./components/pages/main-page/main-page.component")
+        .then((m) => m.MainPageComponent),
   }
-}];
+];
