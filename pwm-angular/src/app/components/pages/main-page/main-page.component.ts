32176fa0b8fce14dc9e036fd641d051e8b2e8f68
TychoQS <tycho.quintana@gmail.com>
2025-04-08 17:32:04 +0100

    implemented main page (load image todo)

--- pwm-angular/src/app/components/pages/main-page/main-page.component.ts
+++ pwm-angular/src/app/components/pages/main-page/main-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {AccordionComponent} from '../../accordion/accordion.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [
+    AccordionComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
+  questions = [
+    "How many owls can I carry to the event?",
+    "How much are the event tickets?",
+    "Will there be an event next year?",
+    "Will there be DJ Santa?"
+  ]
+  answers = [
+    "As many as you want because we are throwing a party!",
+    "You can look at our prices at the tickets section",
+    "Of course, we are looking forward to seeing you next year!",
+    "Yes, he will party as hard as ever!"
+  ]
+  /* TODO -> Load images */
 }
