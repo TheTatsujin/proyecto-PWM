12bb7076b8739f37252fe71205baa26d3817a03a
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-04-08 18:01:21 +0100

    Added artist-page, artist.service, artist.type

---
+++ pwm-angular/src/app/model/artist.type.ts
@@ -0,0 +1,7 @@
+export type Artist = {
+  id: number;
+  name: string;
+  image: string;
+  debutDate: string;
+  debutLocation: string;
+}
--- pwm-angular/src/app/services/artist-data.service.ts
+++ pwm-angular/src/app/services/artist-data.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import {Artist} from '../model/artist.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistDataService {
+  artists: Array<Artist> = [
+    {
+      id: 1,
+      name: "void()",
+      image: "#",
+      debutLocation: "Arquitectura",
+      debutDate: "10-05-2025",
+    },
+    {
+      id: 2,
+      name: "DJ Santa",
+      image: "#",
+      debutLocation: "Telecomunicaciones",
+      debutDate: "11-05-2025",
+    },
+    {
+      id: 3,
+      name: "Pinky Punk",
+      image: "#",
+      debutLocation: "Telecomunicaciones",
+      debutDate: "12-05-2025"
+    },
+    {
+      id: 4,
+      name: "Gothark",
+      image: "#",
+      debutLocation: "Arquitectura",
+      debutDate: "10-05-2025"
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
+
+  getArtistData(){
+    return this.artists;
+  }
 }
