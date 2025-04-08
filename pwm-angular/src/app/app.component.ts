a2859fd763a57429d911cb559d87b4d20fc0e667
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-04-08 17:18:40 +0100

    Before pull

--- pwm-angular/src/app/app.component.ts
+++ pwm-angular/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {FooterComponent} from './components/footer/footer.component';
-import {DropdownComponent} from './components/dropdown/dropdown.component';
-import {LoginFormComponent} from './components/login-form/login-form.component';
-import {RegisterFormComponent} from './components/register-form/register-form.component';
-import {TicketPageComponent} from './components/pages/ticket-page/ticket-page.component';
+import {ArtistBannerComponent} from './components/artist-banner/artist-banner.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    DropdownComponent,
-    TicketPageComponent
+    ArtistBannerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
