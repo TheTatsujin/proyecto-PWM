316429b8aee853412beae9fc77f4221022ffd158
DylanCas16 <dylancastrillon@outlook.com>
2025-04-22 18:55:42 +0100

    Created header and footer query.

--- pwm-angular/src/app/app.component.ts
+++ pwm-angular/src/app/app.component.ts
@@ -1,32 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {SecondHeaderComponent} from './components/second-header/second-header.component';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, SecondHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwm-angular';
 
-/*
-  header: boolean = false;
-  second: boolean = false;
-  footer: boolean = false;
+  header: number = 1;
+  footer: boolean = true;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(param => {
-      this.header = param['header'] === 'true';
-      this.second = param['second'] === 'true';
-      this.footer = param['footer'] === 'true';
-    })
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentRoute = this.getChild(this.activatedRoute);
+
+        currentRoute.queryParams.subscribe(params => {
+          this.header = Number(params['header'] ?? 1);
+          this.footer= params['footer'] === undefined ? true : params['footer'] === 'true';
+        });
+      });
   }
- */
+
+  getChild(route: ActivatedRoute): ActivatedRoute {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
 }
