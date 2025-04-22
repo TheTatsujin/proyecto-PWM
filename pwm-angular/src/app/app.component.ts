801db15ba78c5112f5d0214163ea3200083543f5
DylanCas16 <dylancastrillon@outlook.com>
2025-04-22 17:17:03 +0100

    Page initializated.

--- pwm-angular/src/app/app.component.ts
+++ pwm-angular/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
+import {SecondHeaderComponent} from './components/second-header/second-header.component';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,20 @@ import {FooterComponent} from './components/footer/footer.component';
 export class AppComponent {
   title = 'pwm-angular';
 
+/*
+  header: boolean = false;
+  second: boolean = false;
+  footer: boolean = false;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(param => {
+      this.header = param['header'] === 'true';
+      this.second = param['second'] === 'true';
+      this.footer = param['footer'] === 'true';
+    })
+  }
+ */
 }
