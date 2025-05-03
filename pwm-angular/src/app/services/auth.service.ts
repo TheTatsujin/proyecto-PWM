37caab07c28c560697b50c88c3512f34b3590e24
TychoQS <tycho.quintana@gmail.com>
2025-05-03 21:22:41 +0100

    Added a method to register a user in Firebase authentication

--- pwm-angular/src/app/services/auth.service.ts
+++ pwm-angular/src/app/services/auth.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Auth, createUserWithEmailAndPassword, updateProfile, UserCredential} from "@angular/fire/auth";
+import {UserInterface} from "../model/user.interface";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   isLoggedIn: boolean = false;
-
+  firebaseAuth = inject(Auth);
   constructor() { }
 
   isAuthenticated() {
@@ -18,5 +21,9 @@ export class AuthService {
 
   logout(){
     this.isLoggedIn = false;
+  }
+
+  registerUser(newUser: UserInterface): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(this.firebaseAuth, newUser.email, newUser.password)
   }
 }
