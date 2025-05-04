e07ef5dd0348fd1ae13a66a1aa94212b026d3c8a
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-05-04 11:10:17 +0100

    Updated image hosting

--- pwm-angular/src/app/components/footer/footer.component.ts
+++ pwm-angular/src/app/components/footer/footer.component.ts
@@ -17,14 +17,14 @@ export class FooterComponent {
   @Input() twitterLink: string = "#";
 
   // Icons
-  @Input() instagramIconLink: string = "#";
-  @Input() tiktokIconLink: string = "#";
-  @Input() youtubeIconLink: string = "#";
-  @Input() twitterIconLink: string = "#";
+  @Input() instagramIconLink: string = "https://raw.githubusercontent.com/TheTatsujin/proyecto-PWM/refs/heads/sprint-2/web/images/icons/instagram-icon.svg";
+  @Input() tiktokIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/tiktok-icon.svg";
+  @Input() youtubeIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/youtube-icon.svg";
+  @Input() twitterIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/twitter-icon.svg";
 
-  @Input() telephoneIconLink: string = "#";
-  @Input() messageIconLink: string = "#";
-  @Input() emailIconLink: string = "#";
+  @Input() telephoneIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/telephone-icon.svg";
+  @Input() messageIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/message-icon.svg";
+  @Input() emailIconLink: string = "https://github.com/TheTatsujin/proyecto-PWM/raw/refs/heads/sprint-2/web/images/icons/email-icon.svg";
 
 
   // Contact info
