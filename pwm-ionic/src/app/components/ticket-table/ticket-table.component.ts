01df0ba6240df5fd70aa14a613c24a66053e0d8f
TychoQS <tycho.quintana@gmail.com>
2025-05-05 20:34:30 +0100

    Created ticket table component

---
+++ pwm-ionic/src/app/components/ticket-table/ticket-table.component.ts
@@ -0,0 +1,32 @@
+import {Component, inject, Input} from '@angular/core';
+import {TicketTableService} from '../../services/ticket-table.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-ticket-table',
+  imports: [],
+  templateUrl: './ticket-table.component.html',
+  styleUrl: './ticket-table.component.css',
+  standalone: true
+})
+export class TicketTableComponent {
+  protected tablePrices: any;
+  @Input() currentLocation: string = "";
+  key: string = "";
+  private subscription: Subscription | undefined;
+  ticketTableService = inject(TicketTableService);
+  constructor() {
+  }
+
+  ngOnChanges() {
+    this.subscription?.unsubscribe();
+    this.subscription = this.ticketTableService.getTicketsOf().subscribe(tickets => {
+      this.tablePrices = tickets.find(ticket => ticket.Evento === this.currentLocation);
+      console.log(this.tablePrices);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+}
