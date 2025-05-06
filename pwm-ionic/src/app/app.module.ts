5d5452c547f1786b279d2ac3d04f783d557e529b
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-05-06 18:16:39 +0100

    Second artist-page push

--- pwm-ionic/src/app/app.module.ts
+++ pwm-ionic/src/app/app.module.ts
@@ -13,7 +13,12 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "pwm-firebase-48e3e", appId: "1:953923855442:web:7e6ca0e869a2f1acf2eb8b", storageBucket: "pwm-firebase-48e3e.firebasestorage.app", apiKey: "AIzaSyDXeQluMXOBeVt9Qw351yrEtcOomr2Ofr8", authDomain: "pwm-firebase-48e3e.firebaseapp.com", messagingSenderId: "953923855442" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp({ projectId: "pwm-firebase-48e3e", appId: "1:953923855442:web:7e6ca0e869a2f1acf2eb8b", storageBucket: "pwm-firebase-48e3e.firebasestorage.app", apiKey: "AIzaSyDXeQluMXOBeVt9Qw351yrEtcOomr2Ofr8", authDomain: "pwm-firebase-48e3e.firebaseapp.com", messagingSenderId: "953923855442" })),
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
