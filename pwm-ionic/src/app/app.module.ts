86aa4b8ae24bc18d83849fd35f5d7cea1dda598a
DylanCas16 <dylancastrillon@outlook.com>
2025-05-06 17:32:17 +0100

    Firebase

--- pwm-ionic/src/app/app.module.ts
+++ pwm-ionic/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "pwm-firebase-48e3e", appId: "1:953923855442:web:7e6ca0e869a2f1acf2eb8b", storageBucket: "pwm-firebase-48e3e.firebasestorage.app", apiKey: "AIzaSyDXeQluMXOBeVt9Qw351yrEtcOomr2Ofr8", authDomain: "pwm-firebase-48e3e.firebaseapp.com", messagingSenderId: "953923855442" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
