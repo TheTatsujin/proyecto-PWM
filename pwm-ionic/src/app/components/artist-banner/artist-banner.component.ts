651f4ff322f19b790e9043a3fa387007e2bacfd5
TychoQS <tycho.quintana@gmail.com>
2025-05-20 18:33:01 +0100

    Working on Artist Detail component

--- pwm-ionic/src/app/components/artist-banner/artist-banner.component.ts
+++ pwm-ionic/src/app/components/artist-banner/artist-banner.component.ts
@@ -1,5 +1,15 @@
-import {Component, input} from '@angular/core';
-import {IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from "@ionic/angular/standalone";
+import {Component, input, ViewChild} from '@angular/core';
+import {
+  IonButton, IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle, IonContent, IonHeader, IonIcon, IonInput, IonItem,
+  IonModal, IonTitle, IonToolbar
+} from "@ionic/angular/standalone";
+import {FormsModule} from "@angular/forms";
+import {ArtistDetailComponent} from "../artist-detail/artist-detail.component";
 
 @Component({
   selector: 'app-artist-banner',
@@ -8,14 +18,35 @@ import {IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} f
     IonCardHeader,
     IonCardTitle,
     IonCardSubtitle,
-    IonCardContent
+    IonCardContent,
+    IonModal,
+    IonContent,
+    IonItem,
+    IonInput,
+    IonButton,
+    IonButtons,
+    IonTitle,
+    IonToolbar,
+    IonHeader,
+    FormsModule,
+    IonIcon,
+    ArtistDetailComponent
   ],
   templateUrl: './artist-banner.component.html',
   styleUrl: './artist-banner.component.scss'
 })
 export class ArtistBannerComponent {
+  @ViewChild(IonModal) modal!: IonModal;
   artistName =  input.required<string>();
   artistImage = input.required<string>();
   eventLocation = input.required<string>();
   eventDate = input.required<string>();
+
+  closeModal() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  setFavourite() {
+    this.modal.dismiss();
+  }
 }
